f1769c9bdd0954c6e740a425e6b5c566779757a7
mariosknl <mariosknl@outlook.com>
2022-01-21 15:14:21 +0200

    created the pulse effect for the release date

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -37,28 +37,3 @@ export const childVariants2 = {
     },
   },
 };
-
-// function MovieCard({ key, original_title, overview }) {
-//   return (
-//     <motion.div variants={item} initial="hidden" animate="visible">
-//       <h2>{original_title}</h2>
-//       <p>{overview}</p>
-//     </motion.div>
-//   );
-// }
-
-// export default MovieCard;
-
-/* <motion.div
-                key={id}
-                variants={containerVariants}
-                initial="hidden"
-                animate="visible"
-              >
-                <motion.div variants={childVariants2}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w500/${backdrop_path}`}
-                    alt=""
-                  />
-                </motion.div>
-              </motion.div> */
