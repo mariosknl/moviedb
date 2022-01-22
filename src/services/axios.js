51d7695cad7d7d84b0b369acc9a27ead3c1de518
mariosknl <mariosknl@outlook.com>
2022-01-22 14:20:05 +0200

    deleted unnecessary code

--- src/services/axios.js
+++ src/services/axios.js
@@ -4,7 +4,6 @@ export const getMovie = (inputValue, setMovie) => {
   const baseUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}&query=${inputValue}`;
   if (!inputValue) return;
   axios.get(baseUrl).then(({ data }) => {
-    console.log(data);
     if (data.results.length !== 0) {
       localStorage.setItem("movies", JSON.stringify(data));
       setMovie(data);
