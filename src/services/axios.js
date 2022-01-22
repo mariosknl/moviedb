308dfd81122ca04a6b86ef95cdbcba5da12f970d
mariosknl <mariosknl@outlook.com>
2022-01-22 14:11:24 +0200

    error handling for wrong movie selection

--- src/services/axios.js
+++ src/services/axios.js
@@ -4,8 +4,13 @@ export const getMovie = (inputValue, setMovie) => {
   const baseUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEDB_API_KEY}&query=${inputValue}`;
   if (!inputValue) return;
   axios.get(baseUrl).then(({ data }) => {
-    localStorage.setItem("movies", JSON.stringify(data));
-    setMovie(data);
+    console.log(data);
+    if (data.results.length !== 0) {
+      localStorage.setItem("movies", JSON.stringify(data));
+      setMovie(data);
+    } else {
+      alert("This movie doesn't exit in our database. Please try another one.");
+    }
   });
 };
 
