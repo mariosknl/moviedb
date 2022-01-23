3e8e180e6e43fb51825230edba69446311fe267f
mariosknl <mariosknl@outlook.com>
2022-01-23 19:09:58 +0200

    isolating each to check individual test return no error

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -3,6 +3,7 @@ describe("Landing to the homepage and searching for a movie", () => {
     cy.clearLocalStorageSnapshot();
   });
   beforeEach(() => {
+    cy.visit("/");
     cy.restoreLocalStorage();
   });
   afterEach(() => {
@@ -13,7 +14,7 @@ describe("Landing to the homepage and searching for a movie", () => {
     cy.contains("Welcome to the MovieDb");
   });
   it("contains an input and two buttons", () => {
-    cy.visit("/");
+    cy.get("input[name=searchInput");
     cy.get("input[type=submit]");
     cy.get("button[type=button]");
   });
@@ -22,6 +23,8 @@ describe("Landing to the homepage and searching for a movie", () => {
     cy.get("input[type=submit]").click();
   });
   it("making a request to moviedb API for Lord of the Rings and getting a response.status of 200", () => {
+    cy.get("input[name=searchInput").type("Lord of the Rings");
+    cy.get("input[type=submit]").click();
     cy.request(
       `https://api.themoviedb.org/3/search/movie?api_key=${Cypress.env(
         "api_key"
@@ -32,11 +35,15 @@ describe("Landing to the homepage and searching for a movie", () => {
     });
   });
   it("displays results like The Two Towers", () => {
+    cy.get("input[name=searchInput").type("Lord of the Rings");
+    cy.get("input[type=submit]").click();
     cy.contains("The Two Towers");
   });
   it("displays the release date of each movie after every result is populated", () => {
     // waiting for every result to show in the screen before display the release date.
-    // we are waiting for 25 seconds
+    // we are waiting for 25 seconds until all the animation is complete
+    cy.get("input[name=searchInput").type("Lord of the Rings");
+    cy.get("input[type=submit]").click();
     cy.wait(25000);
     cy.contains("Release Date");
   });
@@ -44,18 +51,31 @@ describe("Landing to the homepage and searching for a movie", () => {
     cy.getLocalStorage("movies");
   });
   it("clicks view movie details and navigates to a new page", () => {
+    cy.get("input[name=searchInput").type("Lord of the Rings");
+    cy.get("input[type=submit]").click();
     cy.get("a").first().click();
   });
   it("changes the url based on the ID of the movie in the API", () => {
+    cy.get("input[name=searchInput").type("Lord of the Rings");
+    cy.get("input[type=submit]").click();
+    cy.get("a").first().click();
     cy.url().should("eq", "http://localhost:3000/movie/121");
   });
   it("shows details for The Two Towers movie", () => {
+    cy.get("input[name=searchInput").type("Lord of the Rings");
+    cy.get("input[type=submit]").click();
+    cy.get("a").first().click();
+    cy.url().should("eq", "http://localhost:3000/movie/121");
     cy.get("img").should("be.visible");
     cy.get("p").first().contains("The Two Towers");
     cy.contains("Frodo");
     cy.contains("Release Date");
   });
   it("click back to see the results again", () => {
+    cy.get("input[name=searchInput").type("Lord of the Rings");
+    cy.get("input[type=submit]").click();
+    cy.get("a").first().click();
+    cy.url().should("eq", "http://localhost:3000/movie/121");
     cy.visit("/");
     cy.getLocalStorage("movies");
     cy.contains("The Two Towers");
