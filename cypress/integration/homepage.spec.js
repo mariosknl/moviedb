2f12c827b5a02933b634d371e0d50650970d2be7
mariosknl <mariosknl@outlook.com>
2022-01-23 09:16:42 +0200

    created tests for homepage

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,15 @@
+describe("Landing to the homepage and searching for a movie", () => {
+  it("Visits the MovieDb homepage", () => {
+    cy.visit("/");
+    cy.contains("Welcome to the MovieDb");
+  });
+  it("contains an input and two buttons", () => {
+    cy.visit("/");
+    cy.get("input[type=submit]");
+    cy.get("button[type=button]");
+  });
+  it("searching Lord of the Rings", () => {
+    cy.get("input[name=searchInput").type("Lord of the Rings");
+    cy.get("input[type=submit]").click();
+  });
+});
